2c3ebca26cc2ca5212c187e7be619008933fcfcb
Solomon Abramelin <roboticforest@protonmail.com>
2024-05-20 10:44:39 -0500

    goooooot

--- src/hooks/useGoat.js
+++ src/hooks/useGoat.js
@@ -1,7 +1,7 @@
 import useSWR from "swr"
 
 export const useGoat = ({ path, params }) => {
-  const url = `/goatcounter?args=${JSON.stringify({ path, params })}`
+  const url = `api/goatcounter?args=${JSON.stringify({ path, params })}`
   const { data } = useSWR(url)
   
   return data
