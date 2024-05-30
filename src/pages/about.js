87cd7826d11c88b7ae0ed8663389a79bd379148e
Solomon Abramelin <roboticforest@protonmail.com>
2024-05-30 18:18:36 -0500

    add about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,6 +26,22 @@ export default () => {
 
         <div className='mt-4 w-[1200px] max-w-[calc(100vw-2rem)]'>
           Artifact Guesser is a game where you guess the country and date of an artifact.{' '}
+          <div className='my-2'>
+            <b>Feature Requests so far</b> (to be implemented soon):
+          </div>
+          <ul className='list-disc ml-8'>
+            <li>Make the map expandable/contractable</li>
+            <li>Improve timeline on desktop</li>
+            <li>Timed round function</li>
+            <li>Multiplayer! (this will take some time lol)</li>
+          </ul>
+          <div className='my-2'>
+            To add more, please head to the <b>#feature-requests</b> channel in the{' '}
+            <a href='https://discord.gg/TaS779hh' className='text-blue-500 hover:underline'>
+              Discord
+            </a>
+            .
+          </div>
         </div>
       </div>
     </Layout>
