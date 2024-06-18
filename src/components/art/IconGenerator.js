efaac76bc75c98928daff2c840bde8507864100a
Solomon Abramelin <roboticforest@protonmail.com>
2024-06-17 22:07:37 -0500

    deeeploy

--- src/components/art/IconGenerator.js
+++ src/components/art/IconGenerator.js
@@ -1,7 +1,6 @@
 import { useState } from "react"
 import {
   GiBastet,
-  GiAncientColumns,
   GiByzantinTemple,
   GiEgyptianUrns,
   GiGreekSphinx,
@@ -9,20 +8,22 @@ import {
   GiWaxTablet,
   GiTemptation
 } from 'react-icons/gi'
+import { PiEyeFill, PiHandEyeFill } from "react-icons/pi"
 
 export const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
 
 export const IconGenerator = props => {
-  const [icon] = useState(getRandomInt(0, 5))
+  const [icon] = useState(getRandomInt(0, 8))
 
   switch (icon) {
     case 1: return <GiByzantinTemple {...props} />
     case 2: return <GiEgyptianUrns {...props} />
-    case 3: return <GiAncientColumns {...props} />
+    case 3: return <PiEyeFill {...props} />
     case 4: return <GiBastet {...props} />
     case 5: return <GiEyeOfHorus {...props} />
     case 6: return <GiWaxTablet {...props} />
     case 7: return <GiTemptation {...props} />
+    case 8: return <PiHandEyeFill {...props} />
     default: return <GiGreekSphinx {...props} />
   }
 }
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,3 +1,4 @@
+import { IconGenerator } from "@/components/art/IconGenerator"
 import { Statistics } from "@/components/dashbaord/Statistics"
 import { Layout } from "@/components/layout/Layout"
 import { MdDashboard } from "react-icons/md"
@@ -11,12 +12,23 @@ export const dashbaordTheme = {
 export default () => {
 
   return (
-    <Layout title='Dashboard' theme={dashbaordTheme} contentCSS={{ fontFamily: 'monospace' }}>
-      <div className='mb-3 mt-[3px] flex items-center' css={{
-         '@media (max-width: 600px)': { marginLeft: 32 },
+    <Layout title='Dashboard' theme={dashbaordTheme} contentCSS={{
+      fontFamily: 'monospace',
+      background: 'linear-gradient(180deg, var(--backgroundColor), var(--backgroundColorDark))',
+      minHeight: '100vh',
+    }}>
+      <div className='mb-3 mt-[3px] flex items-center justify-between' css={{
+         '@media (max-width: 600px)': { marginLeft: 32, marginRight: 32 },
       }}>
-        <MdDashboard className='mr-2'/>
-        Dashbaord
+        <div className='flex items-center'>
+          <MdDashboard className='mr-2'/>
+          Dashbaord
+        </div>
+        <div className='flex items-center'>
+          <IconGenerator className='mr-2' />
+          <IconGenerator className='mr-2' />
+          <IconGenerator className='mr-2' />
+        </div>
       </div>
       <Statistics />
     </Layout>
